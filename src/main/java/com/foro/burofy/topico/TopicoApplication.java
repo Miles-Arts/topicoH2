6f8bd0e2245a6c27931b1fd389c641e7e68465a0
Milton Figueredo Miles <gratno@gmail.com>
2023-05-20 22:02:46 -0500

    Localhost

--- src/main/java/com/foro/burofy/topico/TopicoApplication.java
+++ src/main/java/com/foro/burofy/topico/TopicoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TopicoApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TopicoApplication.class, args);
 	}
